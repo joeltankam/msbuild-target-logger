3b217a6190dcef944a93c48bc3e6bf1ae7d89f27
joeltankam <joel.tankam@gmail.com>
2019-10-14 21:39:20 +0100

    Fix Windows specific tests

--- test/TargetLogger.Tests/StringExtensionsTests.cs
+++ test/TargetLogger.Tests/StringExtensionsTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System.Runtime.InteropServices;
+using NUnit.Framework;
 
 namespace TargetLogger.Tests
 {
@@ -6,12 +7,20 @@ namespace TargetLogger.Tests
     internal sealed class StringExtensionsTests
     {
         [TestCase("/path/to/directory", "directory")]
-        [TestCase(@"\path\to\directory", "directory")]
-        [TestCase(@"C:\path\to\directory", "directory")]
         [TestCase("/path/to/file.extension", "file.extension")]
         public void TestGetPathFileName(string path, string shortName)
         {
             StringAssert.AreEqualIgnoringCase(shortName, path.GetPathFileName());
+        }
+
+        [TestCase(@"\path\to\directory", "directory")]
+        [TestCase(@"C:\path\to\directory", "directory")]
+        public void TestGetPathFileName_WindowsSpecific(string path, string shortName)
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                StringAssert.AreEqualIgnoringCase(shortName, path.GetPathFileName());
+            else
+                Assert.Ignore();
         }
     }
 }
